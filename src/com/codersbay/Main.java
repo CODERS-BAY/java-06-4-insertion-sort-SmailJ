caf992f43cb6f12675d5ca8358e94bf80d72522e
Smail <smail.jusic16@gmail.com>
2020-09-14 10:04:03 +0200

    Umbenannt und überarbeitet

--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -1,33 +1,35 @@
 package com.codersbay;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         int[] array = {19, 5, 220, 1, 78, 3, 15};
         System.out.println("No Insertion Sort");
-        for (int i : array) {
-            System.out.print(i + " ");
+        for (int greatestSortedElement : Arrays.stream(array).toArray()) {
+            System.out.print(greatestSortedElement + " ");
         }
         System.out.println();
 
         insertionSort(array);
 
         System.out.println("With Insertion Sort");
-        for (int i : array) {
+        for (int i : Arrays.stream(array).toArray()) {
             System.out.print(i + " ");
         }
     }
 
-    public static void insertionSort(int array[]) {
-        for (int j = 1; j < array.length; j++) {
-            int key = array[j];
-            int i = j - 1;
-            while ((i > -1) && (array[i] > key)) {
+    public static void insertionSort(int [] array) {
+        for (int startOfUnsorted = 1; startOfUnsorted < array.length; startOfUnsorted++) {
+            int nextUnsortedElement = array[startOfUnsorted];
+            int i = startOfUnsorted - 1;
+            while ((i > -1) && (array[i] > nextUnsortedElement)) {
                 array[i + 1] = array[i];
                 i--;
             }
-            array[i + 1] = key;
+            array[i + 1] = nextUnsortedElement;
         }
 
     }
